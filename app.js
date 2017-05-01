fd203272d5494294f6f883a6b9767d71a1ef396e
Jeff Harris <jeff@jeffreysharris.com>
2017-05-01 16:17:33 -0700

    Update 2017-05-01T23:17:32.179Z

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var FirebaseFramer, HEIGHT, WIDTH, circle, demoDB;
+var FirebaseFramer, HEIGHT, WIDTH, chats, circle, demoDB, i, len, message;
 
 FirebaseFramer = require('firebaseframer').FirebaseFramer;
 
@@ -35,11 +35,25 @@ demoDB = new FirebaseFramer({
   server: "s-usc1c-nss-134.firebaseio.com"
 });
 
+chats = [];
+
 demoDB.get('/messages', function(messages) {
-  var messageArray;
+  var i, len, message, messageArray, results;
   print(messages);
-  return messageArray = _.toArray(messages);
+  messageArray = _.toArray(messages);
+  results = [];
+  for (i = 0, len = messageArray.length; i < len; i++) {
+    message = messageArray[i];
+    results.push(print(message));
+  }
+  return results;
 });
+
+for (i = 0, len = messageArray.length; i < len; i++) {
+  message = messageArray[i];
+  chats.push(new TextLayer(message.name));
+  chats.push(new TextLayer(message.text));
+}
 
 
 },{"firebaseframer":2}],2:[function(require,module,exports){
