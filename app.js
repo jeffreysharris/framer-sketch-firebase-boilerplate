a110323f8f2da54512c4e78d4db306225ef2322c
Jeff Harris <jeff@jeffreysharris.com>
2017-05-01 16:31:36 -0700

    Update 2017-05-01T23:31:35.167Z

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ demoDB.get('/messages', function(messages) {
     message = messageArray[i];
     results.push(line = new TextLayer({
       text: message.name + ": " + message.text,
-      color: "#101010",
+      color: "#d0d0d0",
       font: "14px/20 Helvetica"
     }));
   }
