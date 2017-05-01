18a93b8d771c36b6947b8c3e78b3a1c51a960d96
Jeff Harris <jeff@jeffreysharris.com>
2017-05-01 16:27:05 -0700

    Update 2017-05-01T23:27:03.828Z

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var FirebaseFramer, HEIGHT, WIDTH, chats, circle, demoDB;
+var FirebaseFramer, HEIGHT, WIDTH, circle, demoDB, line;
 
 FirebaseFramer = require('firebaseframer').FirebaseFramer;
 
@@ -35,7 +35,9 @@ demoDB = new FirebaseFramer({
   server: "s-usc1c-nss-134.firebaseio.com"
 });
 
-chats = [];
+line = new TextLayer({
+  text: "dummy"
+});
 
 demoDB.get('/messages', function(messages) {
   var i, len, message, messageArray, results;
