5d48b46fc22af222b0b5c999eea8987fe3baa05d
Jeff Harris <jeff@jeffreysharris.com>
2017-05-01 16:30:33 -0700

    Update 2017-05-01T23:30:32.684Z

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var FirebaseFramer, HEIGHT, WIDTH, circle, demoDB, line;
+var FirebaseFramer, HEIGHT, WIDTH, circle, demoDB;
 
 FirebaseFramer = require('firebaseframer').FirebaseFramer;
 
@@ -35,18 +35,16 @@ demoDB = new FirebaseFramer({
   server: "s-usc1c-nss-134.firebaseio.com"
 });
 
-line = new TextLayer({
-  text: "dummy",
-  font: "-apple-system"
-});
-
 demoDB.get('/messages', function(messages) {
-  var i, len, message, messageArray, results;
+  var i, len, line, message, messageArray, results;
   messageArray = _.toArray(messages);
   results = [];
   for (i = 0, len = messageArray.length; i < len; i++) {
     message = messageArray[i];
-    results.push(print(message.name + ": " + message.text));
+    results.push(line = new TextLayer({
+      text: message.name + ": " + message.text,
+      font: "14px/20 Helvetica"
+    }));
   }
   return results;
 });
