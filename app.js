4e2108bacffaf948184d922041cc94136d33fc79
Jeff Harris <jeff@jeffreysharris.com>
2017-05-01 17:47:10 -0700

    Update 2017-05-02T00:47:05.527Z

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var FirebaseFramer, HEIGHT, WIDTH, circle, demoDB, style, textfield;
+var FirebaseFramer, HEIGHT, WIDTH, circle, demoDB, textfield;
 
 FirebaseFramer = require('firebaseframer').FirebaseFramer;
 
@@ -35,15 +35,17 @@ textfield = new textfield.Input({
   setup: false,
   type: "text",
   x: 50,
-  y: 100,
+  y: 200,
   width: 200,
   height: 60
-}, style = {
+});
+
+textfield.style = {
   fontSize: "14px",
   color: "#d0d0d0",
   fontFamily: "Helvetica",
   border: "1px solid #ffffff"
-});
+};
 
 demoDB = new FirebaseFramer({
   projectID: "framer-sketch-firebase-test",
