48bf7a4af4cc3b7db45ae8e999d504ce0fe98eed
Jeff Harris <jeff@jeffreysharris.com>
2017-05-01 17:52:05 -0700

    Update 2017-05-02T00:51:58.564Z

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var FirebaseFramer, HEIGHT, WIDTH, button, circle, demoDB, textfield;
+var FirebaseFramer, HEIGHT, Input, WIDTH, button, circle, demoDB, textfield;
 
 FirebaseFramer = require('firebaseframer').FirebaseFramer;
 
-textfield = require("inputfield");
+Input = require("inputfield").Input;
 
 WIDTH = Framer.Screen.width;
 
@@ -31,7 +31,7 @@ circle.on(Events.Click, function() {
   return bounce.start();
 });
 
-textfield = new textfield.Input({
+textfield = new Input({
   setup: false,
   type: "text",
   x: 50,
@@ -47,11 +47,12 @@ textfield.style = {
   border: "1px solid #ffffff"
 };
 
-button = new textfield.Input({
+button = new Input({
   setup: false,
   type: "button",
   x: 250,
-  y: 200
+  y: 200,
+  text: "Post"
 });
 
 demoDB = new FirebaseFramer({
