584aeb4de8793814c4d8833f9abaf01971ed46bd
Jeff Harris <jeff@jeffreysharris.com>
2017-05-01 17:49:31 -0700

    Update 2017-05-02T00:49:24.000Z

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var FirebaseFramer, HEIGHT, WIDTH, circle, demoDB, textfield;
+var FirebaseFramer, HEIGHT, WIDTH, button, circle, demoDB, textfield;
 
 FirebaseFramer = require('firebaseframer').FirebaseFramer;
 
@@ -46,6 +46,14 @@ textfield.style = {
   fontFamily: "Helvetica",
   border: "1px solid #ffffff"
 };
+
+button = new textField.Input({
+  setup: false,
+  type: "button",
+  x: 250,
+  y: 200,
+  text: "Post"
+});
 
 demoDB = new FirebaseFramer({
   projectID: "framer-sketch-firebase-test",
