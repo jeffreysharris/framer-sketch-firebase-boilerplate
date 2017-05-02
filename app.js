64edf1ee5a017fa388082dc5c54f617b9c87d515
Jeff Harris <jeff@jeffreysharris.com>
2017-05-02 08:48:27 -0700

    Update 2017-05-02T15:48:26.728Z

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var FirebaseFramer, HEIGHT, Input, WIDTH, bg, button, circle, demoDB, textfield;
+var FirebaseFramer, HEIGHT, Input, WIDTH, bg, button, circle, demoDB, footer, textfield;
 
 FirebaseFramer = require('firebaseframer').FirebaseFramer;
 
@@ -15,6 +15,14 @@ Framer.Defaults.Animation = {
 
 bg = new BackgroundLayer({
   backgroundColor: "#fafafa"
+});
+
+footer = new Layer({
+  x: 0,
+  y: Canvas.height - 250,
+  width: Canvas.width,
+  height: 250,
+  backgroundColor: "#333"
 });
 
 circle = new Layer({
