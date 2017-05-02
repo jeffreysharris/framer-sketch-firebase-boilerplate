3f550f1e616b1ab06db76ad8796a1e2eebe77e0d
Jeff Harris <jeff@jeffreysharris.com>
2017-05-01 17:44:43 -0700

    Update 2017-05-02T00:44:34.893Z

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var FirebaseFramer, HEIGHT, WIDTH, circle, demoDB, textfield;
+var FirebaseFramer, HEIGHT, WIDTH, circle, demoDB, style, textfield;
 
 FirebaseFramer = require('firebaseframer').FirebaseFramer;
 
@@ -35,10 +35,10 @@ textfield = new textfield.Input({
   setup: false,
   type: "text",
   x: 50,
-  y: 500,
+  y: 100,
   width: 200,
   height: 60
-}, input.style = {
+}, style = {
   fontSize: "14px",
   color: "#d0d0d0",
   fontFamily: "Helvetica",
