7c4406225f751f3b4838903f32bef48464169260
Jeff Harris <jeff@jeffreysharris.com>
2017-05-02 22:27:22 -0700

    Update 2017-05-03T05:27:19.062Z

--- app.js
+++ app.js
@@ -20922,6 +20922,7 @@ function wrappy (fn, cb) {
 }
 
 },{}],126:[function(require,module,exports){
+(function (__filename){
 var FirebaseFramer, HEIGHT, Input, WIDTH, bg, button, demoDB, field, footer, fs, lineHeight, path, post, stream, textfield;
 
 fs = require('browserify-fs');
@@ -20937,7 +20938,8 @@ WIDTH = Framer.Screen.width;
 HEIGHT = Framer.Screen.height;
 
 fs.mkdir('/home', function(err) {
-  return console.log(err);
+  console.log(err);
+  return console.log(__filename);
 });
 
 lineHeight = 30;
@@ -21059,6 +21061,8 @@ document.addEventListener('keypress', function(event) {
   }
 });
 
+
+}).call(this,"/src/app.coffee")
 
 },{"browserify-fs":7,"firebaseframer":127,"inputfield":128,"path":96}],127:[function(require,module,exports){
 var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
