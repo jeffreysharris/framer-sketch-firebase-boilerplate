c7d206e9d336704fd903ff17d194c2f959ca5490
Jeff Harris <jeff@jeffreysharris.com>
2017-05-02 21:39:43 -0700

    Update 2017-05-03T04:39:42.904Z

--- app.js
+++ app.js
@@ -20922,8 +20922,7 @@ function wrappy (fn, cb) {
 }
 
 },{}],126:[function(require,module,exports){
-(function (__dirname){
-var FirebaseFramer, HEIGHT, Input, WIDTH, button, demoDB, field, footer, fs, images, lineHeight, path, post, stream, textfield;
+var FirebaseFramer, HEIGHT, Input, WIDTH, button, demoDB, field, footer, fs, lineHeight, path, post, stream, textfield;
 
 fs = require('browserify-fs');
 
@@ -20937,9 +20936,7 @@ WIDTH = Framer.Screen.width;
 
 HEIGHT = Framer.Screen.height;
 
-images = __dirname;
-
-fs.readdir(images, function(err, files) {
+fs.exists("images", function(err, files) {
   return console.log(err);
 });
 
@@ -21060,8 +21057,6 @@ document.addEventListener('keypress', function(event) {
   }
 });
 
-
-}).call(this,"/src")
 
 },{"browserify-fs":7,"firebaseframer":127,"inputfield":128,"path":96}],127:[function(require,module,exports){
 var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
