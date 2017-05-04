aac73b0edf5294b489cc57f89b22f9065de6a198
Jeff Harris <jeff@jeffreysharris.com>
2017-05-04 15:34:38 -0700

    Update 2017-05-04T22:34:36.613Z

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var FirebaseFramer, HEIGHT, Input, WIDTH, _anima, _assets, _slices, anima, asset, constant, constraint, constraints, container, fn, getObject, j, len, lineHeight, prop, ref, slice, slices;
+var FirebaseFramer, HEIGHT, Input, Slice, WIDTH, _anima, _assets, _slices, anima, asset, constant, constraint, constraints, container, fn, getObject, j, len, lineHeight, prop, ref, slice, slices,
+  extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+  hasProp = {}.hasOwnProperty;
 
 FirebaseFramer = require('firebaseframer').FirebaseFramer;
 
@@ -18,6 +20,19 @@ _slices = Utils.domLoadJSONSync("slices.json");
 _assets = Utils.domLoadJSONSync("assets.json");
 
 slices = {};
+
+Slice = (function(superClass) {
+  extend(Slice, superClass);
+
+  function Slice() {
+    return Slice.__super__.constructor.apply(this, arguments);
+  }
+
+  return Slice;
+
+})(Layer({
+  sketch_id: this.sketch_id
+}));
 
 getObject = function(object, key, value) {
   var i, prop, result;
