c05f246ec8bec6bd5691ed05e7276918addd7dc0
Jeff Harris <jeff@jeffreysharris.com>
2017-05-04 15:20:11 -0700

    Update 2017-05-04T22:20:10.512Z

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ for (j = 0, len = ref.length; j < len; j++) {
 }
 
 for (slice in slices) {
-  print(slice.sketch_id);
+  print(slice.name);
   asset = getObject(_assets, "objectID", slice.sketch_id);
   container = Screen;
   if (container == null) {
