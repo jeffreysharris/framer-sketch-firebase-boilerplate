f27101c44e7a9e2f726b527d1526cbfb1739bdff
Jeff Harris <jeff@jeffreysharris.com>
2017-05-04 16:57:08 -0700

    Update 2017-05-04T23:57:06.833Z

--- app.js
+++ app.js
@@ -84,7 +84,6 @@ for (j = 0, len = ref.length; j < len; j++) {
 for (slice in slices) {
   asset = getObject(_assets, "objectID", slices[slice].sketch_id);
   container = (ref1 = slices[slice].parent) != null ? ref1 : Screen;
-  print(container);
   anima = asset.userInfo["com.animaapp.stc-sketch-plugin"];
   constraints = anima.kModelPropertiesKey.constraints;
   if (constraints) {
@@ -111,10 +110,10 @@ for (slice in slices) {
             case "height":
               slices[slice].height = container.height - constant;
               break;
-            case centerHorizontally:
+            case "centerHorizontally":
               slices[slice].x = Align.center(constant);
               break;
-            case centerVertically:
+            case "centerVertically":
               slices[slice].y = Align.center(constant);
               break;
             default:
