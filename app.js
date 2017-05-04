7652b4b0f424ede453c64bebb9f95c042622bdb9
Jeff Harris <jeff@jeffreysharris.com>
2017-05-04 16:16:57 -0700

    Update 2017-05-04T23:16:56.133Z

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ for (j = 0, len = ref.length; j < len; j++) {
 for (slice in slices) {
   asset = getObject(_assets, "objectID", slices[slice].sketch_id);
   container = (ref1 = slices[slice].parent) != null ? ref1 : Screen;
-  container = container.toString;
+  container = String(container);
   print(container);
   anima = asset.userInfo["com.animaapp.stc-sketch-plugin"];
   constraints = anima.kModelPropertiesKey.constraints;
