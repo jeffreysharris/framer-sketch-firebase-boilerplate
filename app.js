88d3f09dd84f65f6ea98aab192fe030afb5d10fd
Jeff Harris <jeff@jeffreysharris.com>
2017-05-04 14:35:22 -0700

    Update 2017-05-04T21:35:21.547Z

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var FirebaseFramer, HEIGHT, Input, WIDTH, _anima, _assets, _slices, anima, asset, constraints, getObject, j, len, lineHeight, ref, slice, slices;
+var FirebaseFramer, HEIGHT, Input, WIDTH, _anima, _assets, _slices, anima, asset, constraint, constraints, getObject, j, len, lineHeight, ref, slice, slices;
 
 FirebaseFramer = require('firebaseframer').FirebaseFramer;
 
@@ -61,7 +61,9 @@ for (j = 0, len = ref.length; j < len; j++) {
   anima = asset.userInfo["com.animaapp.stc-sketch-plugin"];
   constraints = anima.kModelPropertiesKey.constraints;
   if (constraints) {
-    getObject(_anima, constraint);
+    for (constraint in constraints) {
+      getObject(_anima, constraint);
+    }
     break;
   }
   if (anima.kViewTypeKey) {
