c36a3d9d09501f13bb34e0c55be3ff20119df706
Jeff Harris <jeff@jeffreysharris.com>
2017-05-04 14:31:58 -0700

    Update 2017-05-04T21:31:57.095Z

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var FirebaseFramer, HEIGHT, Input, WIDTH, _anima, _assets, _slices, anima, asset, constraints, getObjectbyValue, j, len, lineHeight, ref, slice, slices,
+var FirebaseFramer, HEIGHT, Input, WIDTH, _anima, _assets, _slices, anima, asset, constraints, getObject, j, len, lineHeight, ref, slice, slices,
   slice1 = [].slice;
 
 FirebaseFramer = require('firebaseframer').FirebaseFramer;
@@ -20,7 +20,7 @@ _assets = Utils.domLoadJSONSync("assets.json");
 
 slices = {};
 
-getObjectbyValue = function() {
+getObject = function() {
   var i, key, object, prop, result, value;
   object = arguments[0], key = arguments[1], value = 3 <= arguments.length ? slice1.call(arguments, 2) : [];
   result = null;
