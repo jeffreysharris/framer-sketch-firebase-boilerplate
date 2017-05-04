02855bdf3380808ca9e492d2be77004f4204ffe1
Jeff Harris <jeff@jeffreysharris.com>
2017-05-04 11:17:39 -0700

    Update 2017-05-04T18:17:38.116Z

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ for (i = 0, len = ref.length; i < len; i++) {
 
 print(slices);
 
+print(slices.footer.sketch_id);
+
 
 },{"firebaseframer":2,"inputfield":3}],2:[function(require,module,exports){
 var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
