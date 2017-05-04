f9ddf8e4f32c4b3de3bb9ea9a5ddf96d825ab59b
Jeff Harris <jeff@jeffreysharris.com>
2017-05-04 14:22:57 -0700

    Update 2017-05-04T21:22:56.484Z

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var FirebaseFramer, HEIGHT, Input, WIDTH, _assets, _slices, anima, asset, getObject, j, len, lineHeight, ref, slice, slices;
+var FirebaseFramer, HEIGHT, Input, WIDTH, _anima, _assets, _slices, anima, asset, getObject, j, len, lineHeight, ref, slice, slices;
 
 FirebaseFramer = require('firebaseframer').FirebaseFramer;
 
@@ -10,6 +10,8 @@ WIDTH = Framer.Screen.width;
 HEIGHT = Framer.Screen.height;
 
 lineHeight = 30;
+
+_anima = Utils.domLoadJSONSync("animadictionary.json");
 
 _slices = Utils.domLoadJSONSync("slices.json");
 
