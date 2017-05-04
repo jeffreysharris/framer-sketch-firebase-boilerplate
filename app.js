9a2e14f93ce071a19a05f5e7a413b9a0cb2e7464
Jeff Harris <jeff@jeffreysharris.com>
2017-05-04 12:21:09 -0700

    Update 2017-05-04T19:21:08.204Z

--- app.js
+++ app.js
@@ -54,11 +54,7 @@ for (i = 0, len = ref.length; i < len; i++) {
   });
   slices[slice.name].sketch_id = slice.id;
   asset = getObject(_assets, "objectID", slice.id);
-  if (asset.userInfo["com.animaapp.stc-sketch-plugin"].kModelPropertiesKey.constraints) {
-    console.log(asset.userInfo["com.animaapp.stc-sketch-plugin"].kModelPropertiesKey.constraints);
-  } else {
-
-  }
+  print(asset);
 }
 
 
