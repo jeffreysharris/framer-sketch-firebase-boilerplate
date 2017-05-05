b83c925edb48a8a127b9dc1dc3e545ef6f561146
Jeff Harris <jeff@jeffreysharris.com>
2017-05-04 19:28:53 -0700

    Update 2017-05-05T02:28:46.670Z

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var FirebaseFramer, Input, Slice, _assets, _layers, _slices, anima, asset, constant, constraint, constraints, container, getObject, j, layer, len, ref, ref1, ref2, slice, slices,
+var FirebaseFramer, Input, Slice, _assets, _layers, _slices, anima, asset, child, constant, constraint, constraints, container, getObject, j, layer, len, ref, ref1, ref2, slice, slices,
   extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
   hasProp = {}.hasOwnProperty;
 
@@ -79,14 +79,12 @@ for (slice in slices) {
   asset = getObject(_assets, "objectID", slices[slice].sketch_id);
   layer = getObject(_layers, "id", slices[slice].sketch_id);
   if (layer.layers.length > 0) {
-    slices[slice].addChild((function() {
-      var results;
-      results = [];
-      for (slice in slices) {
-        results.push(getObject(layer.layers, "id", slices[slice].sketch_id));
+    for (slice in slices) {
+      child = getObject(layer.layers, "id", slices[slice].sketch_id);
+      if (child != null) {
+        slices[slice].addChild(child);
       }
-      return results;
-    })());
+    }
   }
   container = (ref1 = slices[slice].parent) != null ? ref1 : Screen;
   anima = asset.userInfo["com.animaapp.stc-sketch-plugin"];
